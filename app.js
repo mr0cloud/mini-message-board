5ac4568ecd75fd0af893dacb14052dbf8c7602c1
mr0cloud <169469785+mr0cloud@users.noreply.github.com>
2026-06-29 17:23:34 +0300

    fixed port number & package.json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 const pageRoutes = require("./routes/pages");
 const assetsPath = path.join(__dirname, "public");
